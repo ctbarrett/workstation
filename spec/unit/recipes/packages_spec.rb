a002be46e3c7cb9cb69c6d65c3f0a9d9016c154a
Craig Barrett <craig.barrett@outreach.io>
2016-12-20 10:58:51 -0800

    fixed unit tests to validate logic, not attribute values

--- spec/unit/recipes/packages_spec.rb
+++ spec/unit/recipes/packages_spec.rb
@@ -7,8 +7,11 @@
 RSpec.describe 'workstation::packages' do
   context 'When all attributes are default' do
     let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new
-      runner.converge(described_recipe)
+      ChefSpec::SoloRunner.new do |node|
+        node.normal['workstation']['packages']['homebrew']['taps'] = ['feather/fall']
+        node.normal['workstation']['packages']['homebrew']['casks'] = ['fizban']
+        node.normal['workstation']['packages']['homebrew']['pkgs'] = ['fireball']
+      end.converge(described_recipe)
     end
 
     before do
@@ -20,78 +23,30 @@ RSpec.describe 'workstation::packages' do
     end
 
     # Homebrew taps
-    %w(
-      caskroom/cask
-      homebrew/completions
-      homebrew/dupes
-    ).each do |tap|
-      it "taps homebrew tap #{tap}" do
-        expect(chef_run).to tap_homebrew_tap(tap.to_s)
-      end
+    it 'taps valid homebrew taps' do
+      expect(chef_run).to tap_homebrew_tap('feather/fall')
+    end
+
+    it 'does not tap invalid/unlisted homebrew taps' do
+      expect(chef_run).to_not tap_homebrew_tap('fall/out')
     end
 
     # Homebrew packages
-    %w(
-      amazon-ecs-cli
-      apm-bash-completion
-      awscli
-      bash-completion
-      bash-git-prompt
-      brew-cask-completion
-      coreutils
-      docker-completion
-      docker-compose-completion
-      gem-completion
-      grep
-      kitchen-completion
-      launchctl-completion
-      mas
-      open-completion
-      packer
-      packer-completion
-      pip-completion
-      python
-      python3
-      rake-completion
-      ruby-completion
-      todo-txt
-      vagrant-completion
-    ).each do |pkg|
-      it "installs homebrew package #{pkg}" do
-        expect(chef_run).to install_homebrew_package(pkg.to_s)
-      end
+    it 'installs valid homebrew packages' do
+      expect(chef_run).to install_homebrew_package('fireball')
+    end
+
+    it 'does not install invalid/unlisted homebrew packages' do
+      expect(chef_run).to_not install_homebrew_package('great_balls_of_fire')
     end
 
     # Homebrew casks
-    %w(
-      atom
-      box-sync
-      caffeine
-      daisydisk
-      docker
-      flux
-      gimp
-      google-chrome
-      google-drive
-      hermes
-      istat-menus
-      iterm2
-      kitematic
-      microsoft-office
-      nosleep
-      skype
-      slack
-      spectacle
-      todotxt
-      utorrent
-      vagrant
-      virtualbox
-      virtualbox-extension-pack
-      zoomus
-    ).each do |cask|
-      it "installs homebrew cask #{cask}" do
-        expect(chef_run).to install_homebrew_cask(cask.to_s)
-      end
+    it 'installs valid homebrew casks' do
+      expect(chef_run).to install_homebrew_cask('fizban')
+    end
+
+    it 'does not install invalid/unlisted homebrew casks' do
+      expect(chef_run).to_not install_homebrew_cask('fizzbuzz')
     end
   end
 end
